6bcca3e10efda307cd0d2812b82e5b2a74365aff
salrashid123 <salrashid123@gmail.com>
2022-10-17 07:53:26 -0400

    add client; offline sign

--- client/main.go
+++ client/main.go
@@ -1,12 +1,17 @@
 package main
 
 import (
+	"bytes"
 	"context"
+	"encoding/base64"
+	"encoding/json"
 	"flag"
 	"fmt"
 	"time"
 
 	//"github.com/google/go-containerregistry/pkg/authn"
+
+	"github.com/go-openapi/runtime"
 	"github.com/google/go-containerregistry/pkg/name"
 	"github.com/google/go-containerregistry/pkg/v1/remote"
 	cosign "github.com/sigstore/cosign/pkg/cosign"
@@ -16,6 +21,8 @@ import (
 	"github.com/sigstore/rekor/pkg/generated/client/entries"
 	"github.com/sigstore/rekor/pkg/generated/client/index"
 	"github.com/sigstore/rekor/pkg/generated/models"
+	"github.com/sigstore/rekor/pkg/types"
+	"github.com/sigstore/sigstore/pkg/signature/payload"
 )
 
 var (
@@ -39,6 +46,8 @@ func main() {
 		panic(err)
 	}
 
+	fmt.Println(">>>>>>>>>> Search rekor <<<<<<<<<<")
+
 	idx, err := rekorClient.Index.SearchIndex(&index.SearchIndexParams{
 		Query: &models.SearchIndex{
 			Hash: *hash,
@@ -60,18 +69,63 @@ func main() {
 
 		for _, en := range e.Payload {
 			fmt.Printf("LogIndex %d\n", *en.LogIndex)
-			fmt.Printf("UUID %s\n", *en.LogID)
-			fmt.Printf("Body %s\n", en.Body)
+			fmt.Printf(" UUID %s\n", *en.LogID)
 
+			// todo find out ow to extract the intoto as go objects from body
+			// https://github.com/sigstore/rekor/tree/main/pkg/types
+			// https://github.com/sigstore/rekor/blob/main/pkg/types/intoto/v0.0.1/entry.go
+
+			b, err := base64.StdEncoding.DecodeString(en.Body.(string))
+			if err != nil {
+				panic(err)
+			}
+
+			pe, err := models.UnmarshalProposedEntry(bytes.NewReader(b), runtime.JSONConsumer())
+			if err != nil {
+				panic(err)
+			}
+			eimpl, err := types.UnmarshalEntry(pe)
+			if err != nil {
+				panic(err)
+			}
+
+			fmt.Printf(" Entry API Version %s\n", eimpl.APIVersion())
+
+			// just parse inttoto types since thats what we submitted
+			it, ok := pe.(*models.Intoto)
+			if !ok {
+				panic(err)
+			}
+			fmt.Printf(" Kind: %s\n", it.Kind())
+
+			var ta models.IntotoV001Schema
+			if err := types.DecodeEntry(it.Spec, &ta); err != nil {
+				panic(err)
+			}
+
+			dec, err := base64.RawStdEncoding.DecodeString(ta.PublicKey.String())
+			if err := types.DecodeEntry(it.Spec, &ta); err != nil {
+				panic(err)
+			}
+			if err := types.DecodeEntry(it.Spec, &ta); err != nil {
+				panic(err)
+			}
+			fmt.Printf(" PublicKey:\n%s\n", dec)
+
+			// this is just to demo:  it verifies an entry is included in the tlog...though we just
+			// got the entry from the tlog in the first place...
 			err = cosign.VerifyTLogEntry(ctx, rekorClient, &en)
 			if err != nil {
 				panic(err)
 			}
-			fmt.Printf("rekor logentry verified\n")
+			fmt.Printf(" rekor logentry inclustion verified\n")
 		}
 	}
 
 	// *******************
+
+	fmt.Println(">>>>>>>>>> Verifying Image Signatures using provided PublicKey <<<<<<<<<<")
+	// cosign verify --key cert/kms_pub.pem       docker.io/salrashid123/myimage:server@sha256:83ab2ba6689713f2d68104cd208feadfebdd6bc881c455dcb55d2b45ac3a0753
 	pubKey, err := sigs.LoadPublicKey(ctx, *kmspub)
 	if err != nil {
 		panic(err)
@@ -94,14 +148,10 @@ func main() {
 	// 	panic(err)
 	// }
 
-	// opts := []remote.Option{
-	// 	remote.WithAuth(&authn.Bearer{
-	// 		Token: t.AccessToken,
-	// 	}),
-	// 	remote.WithContext(ctx),
-	// }
-
 	opts := []remote.Option{
+		// remote.WithAuth(&authn.Bearer{
+		// 	Token: t.AccessToken,
+		// }),
 		remote.WithContext(ctx),
 	}
 
@@ -117,29 +167,26 @@ func main() {
 	}
 
 	if ok {
-		for _, s := range c {
-			bsig, err := s.Base64Signature()
-			if err != nil {
-				panic(err)
-			}
-			fmt.Printf("Signature %s\n", bsig)
-
-			bun, err := s.Bundle()
-			if err != nil {
-				panic(err)
-			}
-			if bun != nil {
-				fmt.Printf("   LogIndex %d\n", bun.Payload.LogIndex)
-
-			}
-
-			// p, err := s.Payload()
-			// if err != nil {
-			// 	panic(err)
-			// }
-			// fmt.Printf("%v\n", string(p))
-
+		fmt.Println("Bundle Verified")
+	}
+	for _, s := range c {
+		bsig, err := s.Base64Signature()
+		if err != nil {
+			panic(err)
 		}
+		fmt.Printf("Verified signature %s\n", bsig)
+
+		p, err := s.Payload()
+		if err != nil {
+			panic(err)
+		}
+		ss := payload.SimpleContainerImage{}
+		if err := json.Unmarshal(p, &ss); err != nil {
+			fmt.Println("error decoding the payload:", err.Error())
+			return
+		}
+		fmt.Printf("  Image Ref %s\n", ss.Critical.Image)
+
 	}
 
 }
